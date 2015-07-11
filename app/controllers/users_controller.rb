ea40e7b036b4a21ed794f853dfb7324dd0b1953b
James Duffy <jduffy3@gmail.com>
2015-07-11 09:56:30 +0100

    Implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update]
   def new
   	@user = User.new
   end
@@ -17,9 +18,27 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile has been updated."
+      redirect_to @user
+    else
+      flash[:alert] = "Profile has not been updated."
+      render "edit"
+    end
+  end
+
   private
 
   	def user_params
   		params.require(:user).permit(:name,:password,:password_confirmation)
   	end
+
+    def set_user
+      #not handling exception- thinking: user for edit has to be logged in, therefore found
+      @user = User.find(params[:id])
+    end
 end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+	factory :user do
+		name "User Name"
+		email "user@example.com"
+		password "password"
+	end
+end
